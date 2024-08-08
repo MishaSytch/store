a2b94904922e674e9bf2c74adb59a6b8c1a86a7f
Misha S. <misha.sytch@mail.ru>
2024-08-08 16:56:00 +0300

    Добавлен контроллер Product

---
+++ src/main/java/store/backend/controller/ProductController.java
@@ -0,0 +1,34 @@
+package store.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import store.backend.database.entity.Product;
+import store.backend.database.repository.ProductRepository;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/{id}")
+    public Product getProduct(@PathVariable("id") Long id) {
+        return productRepository.getById(id);
+    }
+
+    @GetMapping("/{category_id}")
+    public Iterable<Product> getProducts(@PathVariable("category_id") Long category_id) {
+        return productRepository.getProductsByCategory_Id(category_id);
+    }
+
+    @PostMapping
+    public Product postProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable("id") Long id) {
+        productRepository.deleteById(id);
+    }
+
+}
--- src/main/java/store/backend/database/repository/ProductRepository.java
+++ src/main/java/store/backend/database/repository/ProductRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import store.backend.database.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Iterable<Product> getProductsByCategory_Id(Long categoryId);
 }
