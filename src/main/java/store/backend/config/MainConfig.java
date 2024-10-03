16a564c9d6b12ca86790e0885d4d216901bcb37a
Misha S <misha.sytch@mail.ru>
2024-10-03 17:20:39 +0300

    Переменные окружения не работают

--- src/main/java/store/backend/config/MainConfig.java
+++ src/main/java/store/backend/config/MainConfig.java
@@ -1,19 +1,9 @@
 package store.backend.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @ComponentScan(basePackages = {"store/backend"})
-@EnableTransactionManagement
 public class MainConfig {
-
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        return new DataSourceTransactionManager();
-    }
 }
