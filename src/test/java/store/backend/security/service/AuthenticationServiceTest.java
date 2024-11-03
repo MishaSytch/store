bbb173be65fe6087721816cd3f4703cfefbf02c0
Misha S. <misha.sytch@mail.ru>
2024-11-03 14:59:26 +0300

    Создан класс для тестов аутентификации

---
+++ src/test/java/store/backend/security/service/AuthenticationServiceTest.java
@@ -0,0 +1,16 @@
+package store.backend.security.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthenticationServiceTest {
+
+    @Test
+    void signUp() {
+    }
+
+    @Test
+    void signIn() {
+    }
+}
